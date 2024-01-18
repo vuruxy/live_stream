05ac389be707f40fa64566de90fab1f3633df87f
vuruxy <kaunghtetoo07@gmail.com>
2024-01-18 22:54:42 +0800

    check the dynamic path

---
+++ app/(dashboard)/[u]/[username]/layout.tsx
@@ -0,0 +1,17 @@
+//interface CreatorLayoutProps {
+//	params: { username: string };
+//	children: React.ReactNode;
+//};
+
+//const CreatorLayout = ({
+//	params,
+//	children,
+//}: CreatorLayoutProps) => {
+//	return (
+//		<div>
+//			{children}
+//		</div>
+//	);
+//}
+
+//export default CreatorLayout;
